abc9d11d73273f5dfaa62879b84188aa67002e7a
karan@guardrailsai.com <thekaranacharya@gmail.com>
2024-02-21 18:34:54 -0500

    Update code

--- test/test-validator.py
+++ test/test-validator.py
@@ -1,4 +1,4 @@
-# to run these, run 
+# to run these, run
 # pytest test/test-validator.py
 
 from guardrails import Guard
@@ -10,12 +10,14 @@ from validator import EndpointIsReachable
 #  https://www.guardrailsai.com/docs/concepts/output/#%EF%B8%8F-specifying-corrective-actions
 guard = Guard.from_string(validators=[EndpointIsReachable(on_fail="refrain")])
 
+
 def test_pass():
-  test_output = "https://www.google.com"
-  raw_output, guarded_output, *rest = guard.parse(test_output)
-  assert(guarded_output is test_output)
+    test_output = "https://www.google.com"
+    raw_output, guarded_output, *rest = guard.parse(test_output)
+    assert guarded_output is test_output
+
 
 def test_fail():
-  test_output = "https://novalidurl.com"
-  raw_output, guarded_output, *rest = guard.parse(test_output)
-  assert(guarded_output is None)
+    test_output = "https://novalidurl.com"
+    raw_output, guarded_output, *rest = guard.parse(test_output)
+    assert guarded_output is None
